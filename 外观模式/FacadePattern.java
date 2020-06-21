7f99bfd10de97c34e7f45faceb1968f427ce7ef1
zack <919353569@qq.com>
2020-06-21 21:11:42 +0800

    add facade pattern notes

---
+++ 外观模式/FacadePattern.java
@@ -0,0 +1,57 @@
+public class FacadePattern {
+    public interface Shape {
+        void draw();
+    }
+    public class Rectangle implements Shape {
+
+        @Override
+        public void draw() {
+            System.out.println("Rectangle::draw()");
+        }
+    }
+    public class Square implements Shape {
+
+        @Override
+        public void draw() {
+            System.out.println("Square::draw()");
+        }
+    }
+    public class Circle implements Shape {
+
+        @Override
+        public void draw() {
+            System.out.println("Circle::draw()");
+        }
+    }
+    //创建一个外观类。
+    public class ShapeMaker {
+        private Shape circle;
+        private Shape rectangle;
+        private Shape square;
+
+        public ShapeMaker() {
+            circle = new Circle();
+            rectangle = new Rectangle();
+            square = new Square();
+        }
+
+        public void drawCircle(){
+            circle.draw();
+        }
+        public void drawRectangle(){
+            rectangle.draw();
+        }
+        public void drawSquare(){
+            square.draw();
+        }
+    }
+
+    public static void main(String [] args){
+        FacadePattern fp = new FacadePattern();
+        ShapeMaker shapeMaker = fp.new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
